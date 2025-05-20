cc0861231f8d6db75b5935ee00f9c4c01af3594e
JasminF <60253960+40412@users.noreply.github.com>
2025-05-20 20:31:19 +0300

    1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,23 +1,25 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content content={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -26,12 +28,12 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-const Content = ({ content }) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part item={content[0]} />
-      <Part item={content[1]} />
-      <Part item={content[2]} />
+      <Part item={parts[0]} />
+      <Part item={parts[1]} />
+      <Part item={parts[2]} />
     </div>
   );
 };
@@ -44,7 +46,11 @@ const Part = ({ item }) => {
   );
 };
 
-const Total = ({ total }) => {
+const Total = ({ parts }) => {
+  let total = 0;
+  parts.forEach((element) => {
+    total += element.exercises;
+  });
   return <p>Number of exercises {total} </p>;
 };
 
