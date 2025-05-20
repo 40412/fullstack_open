fd2c995dbd5c5147dbd0d3292478ccc4eca12c93
JasminF <60253960+40412@users.noreply.github.com>
2025-05-20 21:08:03 +0300

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [good, setGood] = useState(6);
+  const [neutral, setNeutral] = useState(2);
+  const [bad, setBad] = useState(1);
+  const all = good + neutral + bad;
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button>good</button>
+      <button>neutral</button>
+      <button>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good + bad * -1) / all}</p>
+      <p>positive {good / all} %</p>
+    </div>
+  );
 };
 
 export default App;
