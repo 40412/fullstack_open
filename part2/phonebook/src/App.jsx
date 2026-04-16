644ecf0e087d4102a8812e1ec54ae873cab2d7d3
40412 <jasmin.franti@gmail.com>
2026-04-16 17:09:35 +0300

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
 import { Filter } from "./filter";
 import { AddForm } from "./addForm";
 import { People } from "./people";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -33,7 +35,11 @@ const App = () => {
       return;
     }
 
-    const nameObject = { name: newName, number: newNumber };
+    const nameObject = {
+      name: newName,
+      number: newNumber,
+      id: String(persons.length + 1),
+    };
 
     setPersons(persons.concat(nameObject));
     setNewName("");
