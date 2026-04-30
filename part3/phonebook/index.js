3ff0a508141444b8521aa305048151b8a38ab761
40412 <jasmin.franti@gmail.com>
2026-04-30 16:52:05 +0300

    3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,6 +7,16 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/info", (req, res) => {
+  const count = persons.length;
+  const time = new Date();
+
+  res.send(`
+    <p>Phonebook has info for ${count} people</p>
+    <p>${time}</p>
+  `);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
