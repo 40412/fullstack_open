277ac5bd191f89316bd3b41fef9d928e4ad69be8
40412 <jasmin.franti@gmail.com>
2026-04-30 18:03:52 +0300

    3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,8 +4,14 @@ const app = express();
 
 let persons = require("./data.json");
 
+morgan.token("body", (req) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : "";
+});
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
